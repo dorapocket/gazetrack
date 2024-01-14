b6bcfcdf8219a4d350b1587494cc9fbb24701b01
Dorapocket <993549790@qq.com>
2024-01-14 16:55:56 +0800

    feat: add resent & download

--- src/components/TestView.vue
+++ src/components/TestView.vue
@@ -7,8 +7,7 @@
 import { Modal, Button } from '@arco-design/web-vue';
 import { onMounted, getCurrentInstance } from 'vue';
 const emit = getCurrentInstance().emit;
-onMounted(() => {
-indexedDB.deleteDatabase('localforage');
+function showModalNext() {
     Modal.info({
         title: '分辨率测试',
         content: "在实验开始之前，请先确认您的显示器分辨率是否满足要求。请问您是否能完全看见黑色矩形框？",
@@ -32,6 +31,42 @@ indexedDB.deleteDatabase('localforage');
             emit('button-clicked', true);
         }
     });
+}
+onMounted(() => {
+    let val = localStorage.getItem('value');
+    let isSend = localStorage.getItem('isSend');
+    if (val && isSend) {
+        val = JSON.parse(val);
+        isSend = JSON.parse(isSend);
+        if (isSend) {
+            indexedDB.deleteDatabase('localforage');
+            showModalNext()
+            localStorage.clear();
+        } else {
+            localStorage.clear();
+            Modal.warning({
+                title: '数据恢复',
+                content: "您好，我们检测到您有待发送的数据。是否要重新进行发送？",
+                okText: "发送",
+                cancelText: "我不知道",
+                hideCancel: false,
+                simple: false,
+                onCancel: () => {
+                    showModalNext()
+                },
+                onOk: () => {
+                    window.experimentee = val.experimentee;
+                    window.experiment_report = val.data;
+                    emit('button-clicked', 6);
+                }
+            });
+        }
+    } else {
+        indexedDB.deleteDatabase('localforage');
+        showModalNext()
+        localStorage.clear();
+    }
+
 })
 </script>
 <style scoped>
